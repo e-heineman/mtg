2b0af7c563541ab424a4968e045859603f59cddd
e heineman <eric.h.heineman@gmail.com>
2022-05-28 20:25:31 -0400

    Complete FindCard functionality, API request

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Card = ({ card }) => {
   return (
     <>
-    <img src={card.image_uris.border_crop} alt="Righteous Valkyrie card art"/>
+      <p>Card Name: {card.name}</p>
+      <img src={card.image_uris.border_crop} alt={card.name}/>
     </>
   )
 }
--- src/components/FindCard.js
+++ src/components/FindCard.js
@@ -1,41 +1,32 @@
 import React from 'react'
-import { useState, useEffect } from 'react';
-// import Card from './Card';
+import { useState } from 'react';
+import Card from './Card';
 
 const FindCard = () => {
-  const [result, setResult] = useState([]);
-
-  useEffect(() => {
-    const getCards = async () => {
-      const cardFromServer = await fetchCard()
-      setResult(cardFromServer)
-    }
-
-    getCards()
-  }, [])
+  // const [loading, setLoading] = useState(false)
+  const [submit, setSubmit] = useState(false);
+  const [card, setCard] = useState({});
 
   const fetchCard = async (e) => {
     e.preventDefault();
-    console.log(e.target.s.value);
     const url = `https://api.scryfall.com/cards/named?exact=${e.target.s.value}`;
     const res = await fetch(url);
     const data = await res.json();
-    const array = [];
-    array.push(data);
-    console.log(array);
-    // setResult(array);
-    // console.log(result);
-    return array;
+    setCard(data);
+    setSubmit(true);
   }
 
   return (
     <>
-    <form action='/find' className='search' onSubmit={(e) => fetchCard(e)}>
-      <label htmlFor="s" className='visuallyHidden'>Find a card</label>
-      <input type="search" id="s" name="s" placeholder="Find a card..."/>
-      <input type="submit" value="Search"/>
-    </form>
-    {/* <Card key={result.id} card={result}/> */}
+      <form action='/find' className='search' onSubmit={(e) => fetchCard(e)}>
+        <label htmlFor="s" className='visuallyHidden'>Find a card</label>
+        <input type="search" id="s" name="s" placeholder="Find a card..."/>
+        <input type="submit" value="Search"/>
+      </form>
+      {!submit ? (
+      <p>Search for a card.</p> ) : (
+      <Card key={card.id} card={card}/>
+      )}
     </>
   )
 }
